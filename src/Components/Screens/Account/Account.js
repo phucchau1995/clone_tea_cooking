58ef8811819bb63a841d9eb68565cb14f3944f6f
phucchau <phucchau.freelancer@gmail.com>
2017-06-08 17:57:41 +0700

    add file js in component

---
+++ src/Components/Screens/Account/Account.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    Text, View, StyleSheet
+} from 'react-native';
+
+class Account extends Component {
+    state = {}
+    render() {
+        return (
+              <View><Text>...</Text></View>
+          );
+    }
+}
+
+export default Account;
---
+++ src/Route.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text,View } from 'react-native';
+import {StackNavigator,TabNavigator} from'react-navigation';
+
+import Home from './Components/Screens/Home';
+import Collection from './Components/Screens/Collection';
+import Source from './Components/Screens/Source';
+import Video from './Components/Screens/Video';
+import Account from './Components/Screens/Account';
+
+
